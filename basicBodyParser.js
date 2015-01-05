959e6c8e885a4bbab7cec9a6bfa07c0fba8b25c8
Chuck Russell <chuck.russell@gmail.com>
2015-01-05 15:44:11 -0500

    Committing

---
+++ basicBodyParser.js
@@ -0,0 +1,20 @@
+/**
+ * Created by charlesrussell on 1/5/15.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express()
+    .use(bodyParser())
+    .use(function (req, res) {
+        if (req.body.foo) {
+            res.end('Body parsed! Value of foo: ' + req.body.foo);
+        }
+        else {
+            res.end('Body does not have foo!');
+        }
+    })
+    .use(function (err, req, res, next) {
+        res.end('Invalid body!');
+    })
+    .listen(3000);
